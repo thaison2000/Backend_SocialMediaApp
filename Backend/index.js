f8095e4349a674ddb9ab8fc064b51ed61a5f2a1a
tienanh28122000 <58310317+tienanh28122000@users.noreply.github.com>
2022-06-08 18:44:57 +0700

    them chuc nang tao/sua/xoa bai viet

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 dotenv.config()
 
+const postRoute = require("./routes/post");
+
 mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -17,10 +19,13 @@ mongoose.connect(
 const app = express()
 const PORT = process.env.PORT
 
+//middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors())
+
+app.use("/api/post", postRoute);
 
 app.listen(PORT,()=>{
     console.log(`User Service started on port ${PORT}`)
